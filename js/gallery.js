f2378d9865c451b68da1bc118eb89433eb45493f
hhonghye <honghye194@gmail.com>
2021-07-03 14:36:14 +0900

    gallery 서브페이지

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+
+$.ajax({
+    url: url,
+    dataType: "json",
+    data: {
+        api_key: "f518feda6c446ce47732487a5543d6de",
+        per_page: 50,
+        format: "json",
+        nojsoncallback: 1,
+        user_id: ""
+    }
+
+})
