2b85f7af2a22e70a0916a46fc24117782c61ac38
hhonghye <honghye194@gmail.com>
2021-07-17 03:31:47 +0900

    department organizaion 추가, 수정, rwd 완

---
+++ js/department.js
@@ -0,0 +1,7 @@
+var $organization_ul_li = $(".organization ul li");
+$organization_ul_li.on("mouseenter", function(){
+    $(this).children(".mask").animate({bottom : 0},200);
+});
+$organization_ul_li.on("mouseleave", function(){
+    $(this).children(".mask").animate({bottom : "-100%"},200);
+});
