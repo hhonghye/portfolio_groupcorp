d3d82fac400a6a9bdd5d1438df84bb1ce42db779
hhonghye <honghye194@gmail.com>
2021-07-19 20:37:50 +0900

    main sub visual rwd

--- js/department.js
+++ js/department.js
@@ -1,7 +1,1 @@
-// var $organization_ul_li = $(".organization ul li");
-// $organization_ul_li.on("mouseenter", function(){
-//     $(this).children(".mask").animate({bottom : 0},200);
-// });
-// $organization_ul_li.on("mouseleave", function(){
-//     $(this).children(".mask").animate({bottom : "-100%"},200);
-// });
+// 
